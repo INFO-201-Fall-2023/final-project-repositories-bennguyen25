682b1a7264fc7970b32e7e48f5c3a906482da095
ben nguyen <113154008+bennguyen25@users.noreply.github.com>
2023-06-03 22:28:58 -0700

    Delete datawrang.R

--- datawrang.R
+++
@@ -1,4 +0,0 @@
-library(dplyr)
-#happiness <- read.csv(".csv")
-suicide <- read.csv("death_rate_from_suicides_gho.csv")
-combined_data <- left_join(happiness, suicide, by = c("school_ID" = "school.ID"))
